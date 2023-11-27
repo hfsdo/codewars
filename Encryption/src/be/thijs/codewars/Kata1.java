08c66693c3c4a3e82231634fb488c4971be67521
Thijs <Thijs@DESKTOP-BU99769.mshome.net>
2023-11-27 21:18:34 +0100

    codewars encryption

---
+++ Encryption/src/be/thijs/codewars/Kata1.java
@@ -0,0 +1,46 @@
+package be.thijs.codewars;
+
+//codewars: Simple Encryption #1 - Alternating Split
+
+public class Kata1 {
+	public static String encrypt(final String text, final int n) {
+		if (n<=0 || text.equals("")) { return text; }
+		
+		String val = text;
+		for (int i = 0; i<n; i++) {
+			String odd = "";
+			String even = "";
+			for (int j = 0; j <val.length(); j++) {
+				if (j%2==0) {
+					even += val.substring(j, j+1);
+				} else {
+					odd += val.substring(j, j+1);
+				}
+			}
+			val = odd + even;
+			
+		}
+		return val;   
+	}
+
+	public static String decrypt(final String encryptedText, final int n) {
+		if (n<=0 || encryptedText.equals("")) { return encryptedText; }
+		
+		String val = encryptedText;
+		int half = val.length()/2 + val.length()%2;
+		
+		for (int i = 0; i<n; i++) {
+			String tmp = "";
+			
+			
+			for (int j = 0; j < half; j++) {
+				tmp += val.substring(j+(val.length()/2), j+(val.length()/2)+1) + (val.length()%2!=0&&j==half-1?"":val.substring(j, j+1));
+			}
+			val = tmp;
+		}
+		
+		
+		// Your code here
+		return val;
+	}
+}
---
+++ Encryption/src/be/thijs/codewars/Kata1Test.java
@@ -0,0 +1,33 @@
+package be.thijs.codewars;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Kata1Test {
+
+	@Test
+	  public void testEncrypt() {
+	    // assertEquals("expected", "actual");
+	    assertEquals("This is a test!", Kata1.encrypt("This is a test!", 0));
+	    assertEquals("hsi  etTi sats!", Kata1.encrypt("This is a test!", 1));
+	    assertEquals("s eT ashi tist!", Kata1.encrypt("This is a test!", 2));
+	    assertEquals(" Tah itse sits!", Kata1.encrypt("This is a test!", 3));
+	    assertEquals("This is a test!", Kata1.encrypt("This is a test!", 4));
+	    assertEquals("This is a test!", Kata1.encrypt("This is a test!", -1));
+	    assertEquals("hskt svr neetn!Ti aai eyitrsig", Kata1.encrypt("This kata is very interesting!", 1));
+	  }
+	    
+	  @Test
+	  public void testDecrypt() {
+	    // assertEquals("expected", "actual");
+	    assertEquals("This is a test!", Kata1.decrypt("This is a test!", 0));
+	    assertEquals("This is a test!", Kata1.decrypt("hsi  etTi sats!", 1));
+	    assertEquals("This is a test!", Kata1.decrypt("s eT ashi tist!", 2));
+	    assertEquals("This is a test!", Kata1.decrypt(" Tah itse sits!", 3));
+	    assertEquals("This is a test!", Kata1.decrypt("This is a test!", 4));
+	    assertEquals("This is a test!", Kata1.decrypt("This is a test!", -1));
+	    assertEquals("This kata is very interesting!", Kata1.decrypt("hskt svr neetn!Ti aai eyitrsig", 1));
+	  }
+
+}
