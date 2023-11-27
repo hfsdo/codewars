e884c7dbf83817a52b66ce26e880e2e72d92dbf5
Thijs <Thijs@DESKTOP-BU99769.mshome.net>
2023-11-27 22:05:32 +0100

    Binary Addition Codewars

---
+++ Binary Addition/src/be/thijs/BinaryAddition.java
@@ -0,0 +1,9 @@
+package be.thijs;
+
+public class BinaryAddition {
+	public static String binaryAddition(int a, int b){
+		//your code here
+		
+		return Integer.toBinaryString(a+b);
+	}
+}
---
+++ Binary Addition/src/be/thijs/BinaryAdditionTest.java
@@ -0,0 +1,26 @@
+package be.thijs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BinaryAdditionTest {
+
+	@ParameterizedTest(name = "a = {1}, b = {2}")
+    @CsvSource(textBlock = """
+                   10,    1,   1
+                    1,    0,   1
+                    1,    1,   0
+                  100,    2,   2
+               111111,   51,  12
+    """)
+    @DisplayName("Sample tests")
+    void sampleTests(String expected, int a, int b) {
+        assertEquals(expected, BinaryAddition.binaryAddition(a, b));
+    }
+
+}
